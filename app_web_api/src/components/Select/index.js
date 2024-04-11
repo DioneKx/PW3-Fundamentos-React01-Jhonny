52d2e23b43046aab3c61a2d8fc9efa97763227f1
DioneKx <mjhonnyvieira@gmail.com>
2024-04-11 20:55:57 -0300

    Jhonny: Fetch-Post

--- app_web_api/src/components/Select/index.js
+++ app_web_api/src/components/Select/index.js
@@ -5,16 +5,18 @@ export function Select({
   name,
   options,
   handlerOnChange,
+  value
 }) {
 
   const opt = options.map((e) => {
     return <option value={e.id} key={e.id}>{e.name}</option>
   })
+  
   return (
     <div className="form_control">
       <label htmlFor={name}>{text}</label>
-      <select name={name} id={name}>
-        <option>Selecione uma categoria</option>
+      <select name={name} id={name} onChange={handlerOnChange}>
+        <option value="0" key="0">Selecione uma categoria</option>
         {opt}
       </select>
     </div>
--- app_web_api/src/pages/NewBook/index.js
+++ app_web_api/src/pages/NewBook/index.js
@@ -1,12 +1,15 @@
 import * as React from 'react'
 import { Input } from '../../components/Input'
 import { Select } from '../../components/Select'
+import { useNavigate } from 'react-router-dom'
 
 import './style.css'
 
 export function NewBook() {
 
+  const navigate = useNavigate()
   const [categories, setCategories] = React.useState([])
+  const [book, setBook] = React.useState({})
 
   React.useEffect(() => {
     fetch( //Metodo para requisições, assim como axios manualmente
@@ -26,40 +29,78 @@ export function NewBook() {
       )
   }, []) // O array vazio é o estado inicial (A ser chamado 1°) do useEffect
 
+  function handlerOnChangeBook(event) {
+    setBook({ ...book, [event.target.name]: event.target.value })
+  }
+
+  function handlerOnChangeSelect(event) {
+    setBook({
+      ...book, [event.target.name]: {
+        id: event.target.value,
+        name: event.target.options[event.target.selectedIndex].text
+      }
+    })
+  }
+
+  function handlerOnSubmit(e) {
+    e.preventDefault()
+
+    fetch('http://localhost:5000/books', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(book)
+    }).then(
+      (res) => res.json()
+    ).then(
+      (data) => {
+        console.log(data)
+        navigate('/books', data)
+      }
+    ).catch(
+      (error) => console.log(error)
+    )
+  }
+
   return (
     <section className="newbook_container">
       <h1>Cadastre livro</h1>
 
-      <form>
+      <form onSubmit={handlerOnSubmit}>
         <Input
           type="text"
-          name="nome_livro"
-          id="nome_livro"
+          name="title"
+          id="title"
           placeholder="Digite o titulo do livro"
           text="Digite o titulo do livro"
+          // value={book}
+          handlerOnChange={handlerOnChangeBook}
         />
 
         <Input
           type="text"
-          name="nome_autor"
-          id="nome_autor"
+          name="author"
+          id="author"
           placeholder="Digite o nome do autor"
           text="Digite o nome do autor"
+          handlerOnChange={handlerOnChangeBook}
         />
 
         <Input
           type="text"
-          name="descricao_livro"
-          id="descricao_livro"
+          name="description"
+          id="description"
           placeholder="Digite a descricao do livro"
           text="digite a descricao do livro"
+          handlerOnChange={handlerOnChangeBook}
         />
 
         <Select
-          name="categoria_livro"
-          id="categoria_livro"
+          name="category"
+          id="category"
           options={categories}
-          // handlerOnChange={}
+          handlerOnChange={handlerOnChangeSelect}
           // value={}
           text="testando"
         />
